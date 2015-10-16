69249c0cb3150a9917c138184d4ff4b65a4899d6
lynn.zhu <lynn.zhu@vipshop.com>
2015-10-16 17:04:00 +0800

    first commit

---
+++ YLAnimationLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLAnimationLearn
+//
+//  Created by xuanlin.zhu on 15/10/16.
+//  Copyright (c) 2015年 xuanlin.zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YLAnimationLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLAnimationLearn
+//
+//  Created by xuanlin.zhu on 15/10/16.
+//  Copyright (c) 2015年 xuanlin.zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
