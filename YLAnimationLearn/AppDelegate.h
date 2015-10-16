9a500586bb336708443c96051934dea297857e3f
lynn.zhu <lynn.zhu@vipshop.com>
2015-10-16 17:04:00 +0800

    first commit

---
+++ YLAnimationLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLAnimationLearn
+//
+//  Created by xuanlin.zhu on 15/10/16.
+//  Copyright (c) 2015年 xuanlin.zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLAnimationLearn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YLAnimationLearn
+//
+//  Created by xuanlin.zhu on 15/10/16.
+//  Copyright (c) 2015年 xuanlin.zhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
