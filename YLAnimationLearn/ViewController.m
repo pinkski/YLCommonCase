367798d339ca3f45a47c0d05442dc35455029902
lynn.zhu <lynn.zhu@vipshop.com>
2015-11-03 12:19:40 +0800

    commit sone

--- YLAnimationLearn/ViewController.m
+++ YLAnimationLearn/ViewController.m
@@ -22,6 +22,17 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    NSArray *objIdArr = [[NSArray alloc] init];
+    NSString *target = @"";
+    for (int i = 0; i< objIdArr.count; i++) {
+        
+        if (i == objIdArr.count - 1) {
+            target = [NSString stringWithFormat:@"%@%@", target, objIdArr[i]];
+        } else {
+            target = [NSString stringWithFormat:@"%@%@,", target, objIdArr[i]];
+        }
+    }
 }
 
 @end
